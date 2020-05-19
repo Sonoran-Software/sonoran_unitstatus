67539382774c8a595e6299e516da7ed106d6941a
Nathan C <me@nathanc.tech>
2020-05-19 00:10:37 -0400

    MAJOR: Update to use new config methods

--- unitstatus/CHANGEMEconfig_unitstatus.lua
+++ unitstatus/CHANGEMEconfig_unitstatus.lua
@@ -6,8 +6,8 @@
     Put all needed configuration in this file.
 ]]
 local config = {
+    enabled = false,
     pluginName = "unitstatus", -- name your plugin here
-    pluginVersion = "1.0", -- version of your plugin
     pluginAuthor = "SonoranCAD", -- author
     requiresPlugins = {}, -- required plugins for this plugin to work, separated by commas
     setStatusCommand = "setstatus", -- user command for setting their own status, leave blank to not use
@@ -21,6 +21,6 @@ local config = {
     }
 }
 
--- IMPORTANT: UNCOMMENT THE BELOW LINE ON ACTUAL PLUGINS!
-
---Config.RegisterPluginConfig(config.pluginName, config)
+if config.enabled then
+    Config.RegisterPluginConfig(config.pluginName, config)
+end
--- unitstatus/cl_unitstatus.lua
+++ unitstatus/cl_unitstatus.lua
@@ -8,29 +8,32 @@
     Put all client-side logic in this file.
 ]]
 
-local pluginConfig = Config.plugins["unitstatus"]
+local pluginConfig = Config.GetPluginConfig("unitstatus")
 
+if pluginConfig.enabled then
 
-if pluginConfig.setStatusCommand ~= "" then
-    RegisterCommand(pluginConfig.setStatusCommand, function(source, args, rawCommand)
-        if #args == 1 then
-            if pluginConfig.statusCodes[args[1]] ~= nil then
-                TriggerServerEvent("SonoranCAD::unitstatus:UpdateStatus", args[1])
+    if pluginConfig.setStatusCommand ~= "" then
+        RegisterCommand(pluginConfig.setStatusCommand, function(source, args, rawCommand)
+            if #args == 1 then
+                if pluginConfig.statusCodes[args[1]] ~= nil then
+                    TriggerServerEvent("SonoranCAD::unitstatus:UpdateStatus", args[1])
+                end
+            else
+                print("Invalid arguments.")
             end
-        else
-            print("Invalid arguments.")
-        end
-    end, false)
-    TriggerEvent('chat:addSuggestion', pluginConfig.setStatusCommand, 'Sets your status in the CAD', {
-        { name="Status to set", help="UNAVAILABLE/AVAILABLE/ON_SCENE/ENROUTE/BUSY" }
-    })
-end
-
-RegisterNetEvent("SonoranCAD::unitstatus:StatusUpdate")
-AddEventHandler("SonoranCAD::unitstatus:StatusUpdate", function(apiId, status, success)
-    if success then
-        print("Successfully changed status.")
-    else
-        print("Failed to change status.")
+        end, false)
+        TriggerEvent('chat:addSuggestion', pluginConfig.setStatusCommand, 'Sets your status in the CAD', {
+            { name="Status to set", help="UNAVAILABLE/AVAILABLE/ON_SCENE/ENROUTE/BUSY" }
+        })
     end
-end)
+
+    RegisterNetEvent("SonoranCAD::unitstatus:StatusUpdate")
+    AddEventHandler("SonoranCAD::unitstatus:StatusUpdate", function(apiId, status, success)
+        if success then
+            print("Successfully changed status.")
+        else
+            print("Failed to change status.")
+        end
+    end)
+
+end

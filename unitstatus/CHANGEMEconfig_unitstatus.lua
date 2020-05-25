754ce8734f21dda87116c2286fe7b8d1844a2776
Nathan C <39100023+nathanctech@users.noreply.github.com>
2020-05-24 23:21:02 -0400

    Release/1.1 (#3)
    * MAJOR: Update to use new config methods
    
    * Fixes #2.
    Allow upper/lowercase and numbers for setstatus.
    
    * print cleanup

--- unitstatus/CHANGEMEconfig_unitstatus.lua
+++ unitstatus/CHANGEMEconfig_unitstatus.lua
@@ -6,8 +6,8 @@
     Put all needed configuration in this file.
 ]]
 local config = {
+    enabled = false,
     pluginName = "unitstatus", -- name your plugin here
-    pluginVersion = "1.0", -- version of your plugin
     pluginAuthor = "SonoranCAD", -- author
     requiresPlugins = {}, -- required plugins for this plugin to work, separated by commas
     setStatusCommand = "setstatus", -- user command for setting their own status, leave blank to not use
@@ -18,9 +18,10 @@ local config = {
         ["AVAILABLE"] = 2,
         ["ENROUTE"] = 3,
         ["ON_SCENE"] = 4
-    }
+    },
+    enableAceCheck = true -- restrict command via ace permission
 }
 
--- IMPORTANT: UNCOMMENT THE BELOW LINE ON ACTUAL PLUGINS!
-
---Config.RegisterPluginConfig(config.pluginName, config)
+if config.enabled then
+    Config.RegisterPluginConfig(config.pluginName, config)
+end
